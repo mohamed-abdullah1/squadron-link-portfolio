f28fc8718b940edcc0f821cf410b542a9eea36fc
mohamed-abdullah1 <melsayed506sa@gmail.com>
2024-05-18 12:06:41 +0300

    edit contact btn in home page

--- app/[locale]/page.jsx
+++ app/[locale]/page.jsx
@@ -38,7 +38,10 @@ export default async function Home() {
             </Button>
             {/* </button> */}
             {/* <button className="border-2 rounded-md border-black px-4 py-2  text-sm sm:text-lg md:text-xl"> */}
-            <Button className="text-sm sm:text-lg md:text-xl text-primary border-2 border-primary hover:text-foreground hover:border-foreground px-4 py-6 bg-inherit foreground">
+            <Button
+              variant="outline"
+              className="px-4 py-6 ext-sm sm:text-lg md:text-xl border-2 bg-inherit border-black dark:border-white hover:bg-accent"
+            >
               <Link href="/contact">{t("contactUs")}</Link>
             </Button>
             {/* </button> */}
